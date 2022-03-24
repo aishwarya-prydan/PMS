bcbd07251c08292d885dd8597fadc39816fac398
aishwaryabirewar-prydan <aishwaryabirewar.prydan@gmail.com>
2022-03-25 00:05:02 +0530

    mmmm

--- PMS_Code/src/Navigation/index.js
+++ PMS_Code/src/Navigation/index.js
@@ -61,13 +61,23 @@ const Navigation = () => {
   return (
     // isAppFirstLaunched != null && (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#3B71F3',
+          },
+          headerTitleAlign: 'center',
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}>
+        {/* <Stack.Screen name="Welcome" component={WelcomeScreen} /> */}
         {/* <Stack.Screen name="SignIn" component={SignInScreen} /> */}
         {/* <Stack.Screen name="DatePicker" component={CustomDatePickerInput} /> */}
         {/* <Stack.Screen name="FullPaymentScreen" component={FullPaymentScreen} />
         <Stack.Screen name="MainPaymentScreen" component={MainPaymentScreen} /> */}
-        <Stack.Screen
+        {/* <Stack.Screen
           name="SignIn"
           component={SignInScreen}
           options={{headerShown: false}}
@@ -97,8 +107,12 @@ const Navigation = () => {
           component={NewPasswordScreen}
           options={{headerShown: false}}
         />
-        <Stack.Screen name="LandingPage2" component={LandingPage2} />
-        <Stack.Screen name="Home" component={TabNavigator} />
+        <Stack.Screen name="LandingPage2" component={LandingPage2} /> */}
+        <Stack.Screen
+          name="Home"
+          component={TabNavigator}
+          options={{headerShown: true}}
+        />
         <Stack.Screen name="HireEmployee" component={HireEmployeeScreen} />
         <Stack.Screen name="GiveProject" component={GiveProjectScreen} />
         <Stack.Screen name="EditProfile" component={EditProfileScreen} />
