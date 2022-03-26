12ddf106bc926182f2369ced83dfa0df1a6b95e2
aishwaryabirewar-prydan <aishwaryabirewar.prydan@gmail.com>
2022-03-26 09:23:00 +0530

    developer screen(work in progress)

--- PMS_Code/src/Screens/DeveloperInfoScreen/DeveloperInfoScreen.js
+++ PMS_Code/src/Screens/DeveloperInfoScreen/DeveloperInfoScreen.js
@@ -1,12 +1,52 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 import React from 'react';
 
 const DeveloperInfoScreen = () => {
+  const names = [
+    {
+      index: '1',
+      name: 'Aishwarya',
+    },
+    {
+      index: '2',
+      name: 'Kishan',
+    },
+    {
+      index: '3',
+      name: 'Vedangi',
+    },
+    {
+      index: '4',
+      name: 'Project',
+    },
+  ];
   return (
-    <View>
-      <Text>DeveloperInfoScreen</Text>
-    </View>
+    <FlatList
+      keyExtractor={index => {
+        return index.index;
+      }}
+      data={names}
+      renderItem={element => {
+        console.log(element.item.name);
+        return <Text style={styles.textStyle}>{element.item.name} </Text>;
+      }}
+    />
   );
 };
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 20,
+    padding: 25,
+    backgroundColor: 'blue',
+    margin: 15,
+    color: '#fff',
+  },
+  listStyle: {
+    textAlign: 'center',
+    margin: 20,
+    padding: 10,
+  },
+});
 
 export default DeveloperInfoScreen;
