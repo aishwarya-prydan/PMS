5c628aee95df0b6cd10977f58baae16aafee5890
vedangipatel-prydan <vedupatel810@gmail.com>
2022-03-27 14:33:19 +0530

    UpdateStatus Screen Modify

---
+++ PMS_Code/src/Components/LastStatus/LastStatus.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const LastStatus = () => {
+    return (
+        <View>
+            <Text>LastStatus</Text>
+        </View>
+    );
+};
+
+export default LastStatus;
