196dcc644c9e31b68b948f59024e50cfd63ba6c1
vedangipatel-prydan <vedupatel810@gmail.com>
2022-03-27 15:27:07 +0530

    Last Update Seen adding

---
+++ PMS_Code/src/Components/LastStatusComponent/LastStatusComponent.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+// import LastStatus from 'react-last-status';
+
+const LastStatusComponent = () => {
+    return (
+        <View>
+            <Text>LastStatusComponent</Text>
+        </View>
+    );
+};
+
+export default LastStatusComponent;
--- PMS_Code/src/Screens/UpdateSatusScreen/UpdateSatusScreen.js
+++ PMS_Code/src/Screens/UpdateSatusScreen/UpdateSatusScreen.js
@@ -1,6 +1,7 @@
 import { View, Text, StyleSheet } from 'react-native';
 import React from 'react';
 import ProgressBar from '../../Components/ProgressBar/ProgressBar.js';
+import LastStatus from '../../Components/LastStatusComponent/LastStatusComponent.js';
 
 const data = [{
   percentage: 70,
@@ -12,6 +13,7 @@ export default function UpdateSatusScreen() {
   return (
     <View style={styles.container}>
       {/* <StatusBar hidden /> */}
+      <LastStatus />
       <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', flexWrap: 'wrap', alignItems: 'center' }}>
         {data.map((p, i) => {
           return <ProgressBar key={i} percentage={p.percentage} color={p.color} delay={500 + 100 * i} max={p.max} />
