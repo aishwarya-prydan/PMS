d4e411597bad0e3e9d6f74e67c23fba8328af127
kishankumar-prydan <88204543+kishan-prydan@users.noreply.github.com>
2022-04-03 02:20:58 +0530

    Update index.js

--- PMS_Code/src/Navigation/index.js
+++ PMS_Code/src/Navigation/index.js
@@ -9,7 +9,7 @@ import AuthNavigator from './AuthNavigator';
 import AppNavigator from './AppNavigator';
 
 const Navigation = () => {
-  const [auth, setAuth] = useState(false);
+  const [auth, setAuth] = useState(true);
   return (
     <NavigationContainer>
       {/* <AuthNavigator /> */}
