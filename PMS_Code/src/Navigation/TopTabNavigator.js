ab4119e689fff31f7ec204b3c30a394358a15e6b
aishwaryabirewar-prydan <aishwaryabirewar.prydan@gmail.com>
2022-04-22 15:19:30 +0530

    nothung

--- PMS_Code/src/Navigation/TopTabNavigator.js
+++ PMS_Code/src/Navigation/TopTabNavigator.js
@@ -1,6 +1,6 @@
-import { View, Text } from 'react-native';
+import {View, Text} from 'react-native';
 import React from 'react';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 
 import MainProjectScreen from '../Screens/MainProjectScreen/MainProjectScreen.js';
 import DeveloperInfoScreen from '../Screens/DeveloperInfoScreen/DeveloperInfoScreen';
@@ -9,16 +9,15 @@ import GiveProjectScreen from '../Screens/GiveProject/GiveProjectScreen.js';
 
 const TopTab = createMaterialTopTabNavigator();
 const TopTabNavigator = () => {
-  
   return (
     <TopTab.Navigator
       screenOptions={{
         // tabBarLabelStyle: {fontSize: 13},
         // tabBarItemStyle: {height: 45, marginBottom: 2, alignSelf: 'center'},
-        tabBarLabelStyle: {fontSize: 13},
+        tabBarLabelStyle: {fontSize: 12, fontWeight: '600'},
         tabBarItemStyle: {
           height: 60,
-          marginBottom: 4,
+          // marginBottom: 4,
           width: 100,
           alignSelf: 'center',
         },
